bd3d9158ce9ae3ef7cc936ed82ea56365ef2f6b4
Magno Belloni <bellonidesouza6@gmail.com>
2020-07-06 19:32:37 -0300

    Resolução do desafio.

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -29,6 +29,8 @@ export const Container = styled.div<ContainerProps>`
 
         &:hover {
           opacity: 0.6;
+          border-bottom: 1px solid #ff872c;
+          padding-bottom: 15px;
         }
       }
     }
